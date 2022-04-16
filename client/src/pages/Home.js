e58a7bc6363a009dd7d2e303543c701b2b4895f5
Joseph Dugan <Joseph.Michael.Dugan@gmail.com>
2022-04-16 12:31:38 -0600

    created client side files

---
+++ client/src/pages/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import mainPhoto from "../assets/images/good-friends.jpg";
+
+const Home = () => {
+  return (
+    <>
+      <container>
+        <img className="blur" src={mainPhoto} alt=""></img>
+      </container>
+    </>
+  );
+};
+
+export default Home;
