393eea40a50ecaf1b2f78ed3fbd1230a89e38890
Matthew Todor <unclejunglegeorge@gmail.com>
2022-04-16 22:39:47 -0600

    started with user model, added comments

--- server/models/User.js
+++ server/models/User.js
@@ -1,8 +1,43 @@
 const { Schema, model } = require("mongoose");
 
-const userSchema = new Schema({
-
-});
+const userSchema = new Schema(
+	// Other possibilities: Gender, Age, Location, Looking for (not hobbies), picture,
+	{
+		firstName: {
+			type: String,
+			required: true,
+			trim: true,
+		},
+		lastName: {
+			type: String,
+			required: true,
+			trim: true,
+		},
+		email: {
+			type: String,
+			required: true,
+			trim: true,
+		},
+		password: {
+			type: String,
+			required: true,
+			trim: true,
+		},
+		about: {
+			type: String,
+			required: false,
+			trim: true,
+		},
+		friends: [{}],
+		hobbies: [{}],
+	},
+	{
+		toJson: {
+			virtuals: true,
+		},
+		id: false,
+	}
+);
 
 const User = model("User", userSchema);
 
