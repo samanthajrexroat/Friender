62a2c0a914ab5a58867f1ed61c9811d32de880a3
Samantha Rexroat <samanthajrexroat@gmail.com>
2022-04-18 13:53:57 -0600

    Garbage

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,10 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
 import "./assets/index.css";
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
+ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
