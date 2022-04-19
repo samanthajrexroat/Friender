ca5f7e99ba372e21372e13aeb5862c146883629d
Matthew Todor <unclejunglegeorge@gmail.com>
2022-04-19 17:15:41 -0600

    fleshed out seed to be more coherent

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -5,29 +5,27 @@ import FooterSocial from "./FooterSocial";
 import CommentsData from "./commentsData";
 
 const Footer = () => {
-  return (
-    <footer>
-      <div className="footerContainer">
-        <div className="commentContainer">
-          {/* <div className="commentCard"> */}
-          {CommentsData.map(({ id, name, comments }) => {
-            return (
-              <>
-                <div className="commentCard" key={id}>
-                  <h5>{name}</h5>
+	return (
+		<footer>
+			<div className="footerContainer">
+				<div className="commentContainer">
+					{/* <div className="commentCard"> */}
+					{CommentsData.map(({ id, name, comments }) => {
+						return (
+							<div className="commentCard" key={id}>
+								<h5>{name}</h5>
 
-                  <p>{comments}</p>
-                </div>
-              </>
-            );
-          })}
-          {/* </div> */}
-        </div>
-      </div>
-      <hr />
-      <FooterSocial />
-    </footer>
-  );
+								<p>{comments}</p>
+							</div>
+						);
+					})}
+					{/* </div> */}
+				</div>
+			</div>
+			<hr />
+			<FooterSocial />
+		</footer>
+	);
 };
 
 export default Footer;
