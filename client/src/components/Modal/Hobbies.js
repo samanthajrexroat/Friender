80a5169bbfff0257914a101e3339e8d675a2f9e9
Joseph Dugan <Joseph.Michael.Dugan@gmail.com>
2022-04-20 17:01:31 -0600

    hobby search works

--- client/src/components/Modal/Hobbies.js
+++ client/src/components/Modal/Hobbies.js
@@ -1,55 +1,79 @@
 import React from "react";
 import "./modal.css";
+import HobbyData from "./HobbyData";
 // import Home from "../../pages/Home";
 // import { useState } from "react";
 import { Link } from "react-router-dom";
-// import useState from "react";
+
+import { useState } from "react";
 // import { ADD_HOBBY } from "../../utils/mutations";
 // import { useMutation } from "@apollo/client";
 
-const Hobbies = ({ userId }) => {
+const Hobbies = () => {
   // const [hobby, setHobby] = useState("");
-  // const [addHobby] = useMutation(ADD_HOBBY);
+  // console.log(hobby);
 
-  // const handleFormSubmit = async event => {
-  //   event.preventDefault();
-  //   console.log("submitted");
-
-  //   try {
-  //     const data = await addHobby({
-  //       variables: { userId, hobby },
-  //     });
-
-  //     setHobby("");
-  //   } catch (err) {
-  //     console.error(err);
-  //   }
-  //   alert("Hobby added");
+  // let searchInput = e => {
+  //   setHobby(e.target.value);
   // };
+
+  const [searchTerm, setSearchTerm] = useState("");
 
   return (
     <div className="profileBackground">
-      <div className="signInModal">
+      <div className="hobbyModal">
         <Link to="/me">
           <div className="closeIcon">ⓧ</div>
         </Link>
-        <h2>Add A Hobby</h2>
-        <p>one at a time</p>
+        <h2>Search for a Hobby</h2>
+        <h5 className="hobbiesContainer" key={HobbyData.id}>
+          {HobbyData.filter(val => {
+            if (searchTerm === "") {
+              return val;
+            } else if (
+              val.hobby.toLowerCase().includes(searchTerm.toLowerCase())
+            ) {
+              return val;
+            }
+          }).map(({ id, hobby }) => {
+            return (
+              <div className="hobbyCard" key={id} value={hobby} name={hobby}>
+                {hobby}
+              </div>
+            );
+          })}
+        </h5>
+
         <form className="logInForm">
           <input
             className="rounded-input"
             type="text"
-            id="hobby"
+            id="hobbyInput"
             name="hobby"
-            placeholder="Hobby"
+            placeholder="Search for a Hobby"
             required={true}
+            // onChange={e => setHobby(e.target.value)}
+            onChange={event => {
+              setSearchTerm(event.target.value);
+            }}
           />
           <Link to="/AddHobbies">
             <button className="secondary-btn" type="submit">
-              Add Hobby
+              Search Hobbies
             </button>
           </Link>
         </form>
+        <p>
+          Dont see your hobby? <br />
+          <a
+            href="mailto:AddHobby@Friender.com"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Email us
+          </a>
+          ! We will add it!
+        </p>
       </div>
     </div>
   );
