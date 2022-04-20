1c38fdf0da29dc67ce0152da2ce5c57a7ab94510
Joseph Dugan <Joseph.Michael.Dugan@gmail.com>
2022-04-20 14:31:03 -0600

    idk

--- client/src/components/Modal/Hobbies.js
+++ client/src/components/Modal/Hobbies.js
@@ -3,29 +3,29 @@ import "./modal.css";
 // import Home from "../../pages/Home";
 // import { useState } from "react";
 import { Link } from "react-router-dom";
-import useState from "react";
-import { ADD_HOBBY } from "../../utils/mutations";
-import { useMutation } from "@apollo/client";
+// import useState from "react";
+// import { ADD_HOBBY } from "../../utils/mutations";
+// import { useMutation } from "@apollo/client";
 
 const Hobbies = ({ userId }) => {
-  const [hobby, setHobby] = useState("");
-  const [addHobby] = useMutation(ADD_HOBBY);
+  // const [hobby, setHobby] = useState("");
+  // const [addHobby] = useMutation(ADD_HOBBY);
 
-  const handleFormSubmit = async event => {
-    event.preventDefault();
-    console.log("submitted");
+  // const handleFormSubmit = async event => {
+  //   event.preventDefault();
+  //   console.log("submitted");
 
-    try {
-      const data = await addHobby({
-        variables: { userId, hobby },
-      });
+  //   try {
+  //     const data = await addHobby({
+  //       variables: { userId, hobby },
+  //     });
 
-      setHobby("");
-    } catch (err) {
-      console.error(err);
-    }
-    alert("Hobby added");
-  };
+  //     setHobby("");
+  //   } catch (err) {
+  //     console.error(err);
+  //   }
+  //   alert("Hobby added");
+  // };
 
   return (
     <div className="profileBackground">
@@ -35,15 +35,13 @@ const Hobbies = ({ userId }) => {
         </Link>
         <h2>Add A Hobby</h2>
         <p>one at a time</p>
-        <form className="logInForm" onSubmit={handleFormSubmit}>
+        <form className="logInForm">
           <input
             className="rounded-input"
             type="text"
             id="hobby"
             name="hobby"
             placeholder="Hobby"
-            value={hobby}
-            onChange={event => setHobby(event.target.value)}
             required={true}
           />
           <Link to="/AddHobbies">
