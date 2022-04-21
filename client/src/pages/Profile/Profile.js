1b10415c351afbaca7e1e219d7dda31d3edaeea6
Matthew Todor <unclejunglegeorge@gmail.com>
2022-04-21 11:48:46 -0600

    added photo functionality to signup and profile page

--- client/src/pages/Profile/Profile.js
+++ client/src/pages/Profile/Profile.js
@@ -16,73 +16,68 @@ import LogIn from "../../components/Modal/LogIn";
 // ];
 
 const Profile = () => {
-  const { userId } = useParams();
+	const { userId } = useParams();
 
-  const { loading, data } = useQuery(userId ? QUERY_USER : QUERY_ME, {
-    variables: { userId: userId },
-  });
+	const { loading, data } = useQuery(userId ? QUERY_USER : QUERY_ME, {
+		variables: { userId: userId },
+	});
 
-  const user = data?.me || data?.user || {};
+	const user = data?.me || data?.user || {};
 
-  if (Auth.loggedIn() && Auth.getProfile().data._id === userId) {
-    return <Navigate to="/me" />;
-  }
+	if (Auth.loggedIn() && Auth.getProfile().data._id === userId) {
+		return <Navigate to="/me" />;
+	}
 
-  if (loading) {
-    return <div>Loading...</div>;
-  }
+	if (loading) {
+		return <div>Loading...</div>;
+	}
 
-  if (!user?._id) {
-    return (
-      <>
-        <h4 className="logInError ">
-          You need to be logged in to see this. Use the navigation links above
-          to sign up or log in!
-        </h4>
-        <LogIn />
-      </>
-    );
-  }
+	if (!user?._id) {
+		return (
+			<>
+				<h4 className="logInError ">You need to be logged in to see this. Use the navigation links above to sign up or log in!</h4>
+				<LogIn />
+			</>
+		);
+	}
 
-  return (
-    <div className="profileBackground">
-      <div className="profileContainer">
-        <Search />
+	return (
+		<div className="profileBackground">
+			<div className="profileContainer">
+				<Search />
 
-        <div className="profileCard">
-          <h2>{user.firstName}</h2>
+				<div className="profileCard">
+					<h2>{user.firstName}</h2>
 
+					<div className="profile">
+						<div className="img-container">
+							<img src={user.photo} alt={"photo of " + user.first_name} />
+						</div>
+					</div>
 
-          <div className="profile">
-                <div className="img-container">
-                    <img src={user.url} alt={"photo of " + user.first_name}/>
-                </div>
-            </div>
-
-
-          <h4>{user.city}</h4>
-          {/* <h5>{gender}</h5> */}
-          <h5>{user.age}</h5>
-          <h5>{user.description}</h5>
-          {/* <div>
+					<h4>{user.city}</h4>
+					{/* <h5>{gender}</h5> */}
+					<h5>{user.age}</h5>
+					<h5>{user.description}</h5>
+					{/* <div>
                 <h5 className="hobbiesContainer">
                   {hobbies.map(hobby => (
                     <div className="hobbiesList">{hobby}</div>
                   ))}
                 </h5>
               </div> */}
-          <div>
-            <Link to="/AddHobbies">
-              <button className="sm-btn">Add hobbies</button>
-            </Link>
-            <Link to="/EditProfile">
-              <button className="sm-btn">edit profile</button>
-            </Link>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+					<div>
+						<Link to="/AddHobbies">
+							<button className="sm-btn">Add hobbies</button>
+						</Link>
+						<Link to="/EditProfile">
+							<button className="sm-btn">edit profile</button>
+						</Link>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Profile;
