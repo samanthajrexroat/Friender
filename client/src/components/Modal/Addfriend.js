ad98ab9ec56d30ba65620ebbd3761b9667fe4fad
Matthew Todor <unclejunglegeorge@gmail.com>
2022-04-22 13:07:42 -0600

    added functionality to the add friend button to add a user to the friend array of the logged in user

--- client/src/components/Modal/Addfriend.js
+++ client/src/components/Modal/Addfriend.js
@@ -3,16 +3,33 @@ import "./modal.css";
 import { useMutation } from "@apollo/client";
 import { ADD_FRIEND } from "../../utils/mutations";
 import UserHobbies from "./UserHobbies";
-
+import Auth from "../../utils/auth";
 const AddFriend = (hobbyFanId) => {
-	const { loading, error, data } = useMutation(ADD_FRIEND, {
-		variables: { friendId: hobbyFanId },
-	});
+	const [addFriend, { loading, error, data }] = useMutation(ADD_FRIEND);
 	if (error) {
 		console.log(JSON.stringify(error));
 	}
-    
-	return <button className="secondary-btn" onClick={}>Add Friend</button>;
+	const userId = Auth.getProfile().data._id;
+	const handleClick = async (hobbyFanId) => {
+		try {
+			const { data } = await addFriend({
+				variables: { userId: userId, friendId: hobbyFanId.hobbyFanId },
+			});
+		} catch (error) {
+			throw error;
+		}
+	};
+
+	return (
+		<div
+			className="secondary-btn"
+			onClick={() => {
+				handleClick(hobbyFanId);
+			}}
+		>
+			Add Friend
+		</div>
+	);
 };
 
 export default AddFriend;
