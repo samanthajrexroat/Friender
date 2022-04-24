b8b5fe8e69fde6f04a231233949ad8dd5a231910
Joseph Dugan <Joseph.Michael.Dugan@gmail.com>
2022-04-24 12:46:01 -0600

    heroku

--- server/server.js
+++ server/server.js
@@ -16,7 +16,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../client/build")));
+  // app.use(express.static(path.join(__dirname, "../client/build")));
+  app.use(express.static("client/build"));
 }
 
 app.get("/", (req, res) => {
