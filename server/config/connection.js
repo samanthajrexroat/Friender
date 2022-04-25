6548f755d99779db40f5b44fab2167927c2a26a3
Matthew Todor <unclejunglegeorge@gmail.com>
2022-04-25 09:29:41 -0600

    asdf

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
-
+//comment
 mongoose.connect(`mongodb+srv://root:root@cluster0.o20dk.mongodb.net/friender_DB?retryWrites=true&w=majority`, {
 	useNewUrlParser: true,
 	useUnifiedTopology: true,
