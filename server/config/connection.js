8ccec7cc47e6e98a0563b40ab4db3809f7b3dffc
Matthew Todor <unclejunglegeorge@gmail.com>
2022-04-25 09:24:42 -0600

    added potiential fix

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,5 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
-console.log(require("dotenv").config());
 
 mongoose.connect(`mongodb+srv://root:root@cluster0.o20dk.mongodb.net/friender_DB?retryWrites=true&w=majority`, {
 	useNewUrlParser: true,
