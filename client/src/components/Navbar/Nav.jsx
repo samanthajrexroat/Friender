822a5bc69513671af0fab8e8d8c0b95020a2c316
Joseph Dugan <Joseph.Michael.Dugan@gmail.com>
2022-06-15 11:16:16 -0600

    updated profile card

--- client/src/components/Navbar/Nav.jsx
+++ client/src/components/Navbar/Nav.jsx
@@ -5,6 +5,7 @@ import { Link, useNavigate } from "react-router-dom";
 import { Button } from "../Button";
 import { SearchBar } from "../../components/SearchBar";
 import Auth from "../../utils/auth";
+import Grid from "@mui/material/Grid";
 
 const Nav = () => {
   // const authToken = true;
@@ -18,48 +19,46 @@ const Nav = () => {
   let navigate = useNavigate();
 
   return (
-    <nav className="navInline">
-      <div className="logoContainer">
-        <h1>
-          <TiThMenuOutline /> Frendr{" "}
-        </h1>
-      </div>
-      <div className="inline">
-        {Auth.loggedIn() ? (
-          <>
-            <Link to="/me">
-              <Button value="View Profile" type="button" />
-            </Link>
+    <Grid container>
+      <nav className="navInline">
+        <div className="logoContainer">
+          <h1>
+            <TiThMenuOutline /> Frendr{" "}
+          </h1>
+        </div>
+        <div className="inline">
+          {Auth.loggedIn() ? (
+            <>
+              <Link to="/me">
+                <Button value="View Profile" type="button" />
+              </Link>
 
-            <Link to="/">
-              <Button
-                value="Logout"
-                onClick={() => {
-                  Auth.logout();
-                  navigate("/", { replace: true });
-                }}
-                type="button"
-              />
-            </Link>
-            <SearchBar
-              value="value"
-              placeholder="Search Friends or Hobbies..."
-              type="input"
-            />
-          </>
-        ) : (
-          <>
-            <Link to="/LogIn">
-              <Button value="LogIn" type="button" />
-            </Link>
+              <Link to="/">
+                <Button
+                  value="Logout"
+                  onClick={() => {
+                    Auth.logout();
+                    navigate("/", { replace: true });
+                  }}
+                  type="button"
+                />
+              </Link>
+              <SearchBar placeholder="Search Friends or Hobbies..." />
+            </>
+          ) : (
+            <>
+              <Link to="/LogIn">
+                <Button value="LogIn" type="button" />
+              </Link>
 
-            <Link to="/SignUp">
-              <Button value="Signup" type="button" />
-            </Link>
-          </>
-        )}
-      </div>
-    </nav>
+              <Link to="/SignUp">
+                <Button value="Signup" type="button" />
+              </Link>
+            </>
+          )}
+        </div>
+      </nav>
+    </Grid>
   );
 };
 
