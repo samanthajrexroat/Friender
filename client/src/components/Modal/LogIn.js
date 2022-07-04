f4e1c20cbf933561bbea7f948df5107e25578342
Matthew Todor <unclejunglegeorge@gmail.com>
2022-07-03 19:25:18 -0600

    trying to clean up error message

--- client/src/components/Modal/LogIn.js
+++ client/src/components/Modal/LogIn.js
@@ -35,7 +35,8 @@ const LogIn = props => {
       // alert(JSON.stringify(data))
     } catch (e) {
       // alert("No user found with that information!");
-      console.error(e);
+      console.table(e);
+      console.log(e);
     }
 
     setFormState({
