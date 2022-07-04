cbd3be25814196f80d6969f37c5e44e8407aa2ee
Matthew Todor <unclejunglegeorge@gmail.com>
2022-07-03 18:58:26 -0600

    fix for unhandled promise rejection warning from mongoose

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
-mongoose.connect('localhost:27017', {
+mongoose.connect('mongodb://localhost:27017', {
 	useNewUrlParser: true,
 	useUnifiedTopology: true,
 });
