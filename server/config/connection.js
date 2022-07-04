0692bbcfb4f8b3e93d63ce2eac5327859b08b004
Matthew Todor <unclejunglegeorge@gmail.com>
2022-07-04 12:54:32 -0600

    waking back up

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
-mongoose.connect(process.env.DB_URL, {
+mongoose.connect(`${process.env.DATABASE_URL}`, {
 	useNewUrlParser: true,
 	useUnifiedTopology: true,
 });
